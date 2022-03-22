ecd9405d07d2499feb67defa347b3050109c543f
Vitor <vitormuller66@gmail.com>
2022-03-22 10:44:04 -0300

    feat: initial setup

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import "./Styles/reset.css"
+
+function app() {
+    return (<h1>Hello World</h1>)
+}
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import reactDom from "react-dom";
+import App from "./app";
+
+reactDom.render(<App />, document.querySelector('.root'))
