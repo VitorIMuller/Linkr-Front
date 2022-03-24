f70c31e0741ae8c97484b386688038843083efb7
Vitor <vitormuller66@gmail.com>
2022-03-24 16:01:28 -0300

    feat: deploy

--- src/Services/signIn.js
+++ src/Services/signIn.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://apilinkr.herokuapp.com/";
 
 function signIn(formData) {
     const promise = axios.post(`${BASE_URL}/sign-in`, formData)
