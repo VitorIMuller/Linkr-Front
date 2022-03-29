3a32a34e75be806239e4be4fd3ecd26c9c547060
Vitor <vitormuller66@gmail.com>
2022-03-29 15:10:10 -0300

    merge

--- src/Pages/UserPage/index.js
+++ src/Pages/UserPage/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { MainContainer, TitleContainer, LeftWrapper ,RightWrapper } from "./style";
+import { MainContainer, TitleContainer, LeftWrapper, RightWrapper } from "./style";
 import Post from "../../Components/Post";
 import useAuth from "../../Hooks/useAuth";
 import Header from "../../Components/Header";
@@ -51,25 +51,25 @@ export default function UserPage() {
                         isLoading
                             ? "Loading..."
                             : posts?.length === 0
-                            ? "There are no posts yet"
-                            : error === true
-                            ? "An error occured while trying to fetch the posts, please refresh the page"
-                            :
-                            (
-                                posts?.map((post) =>
-                                    <Post
-                                        key={post.id}
-                                        postId={post.id}
-                                        url={post.url}
-                                        title={post.urlTitle}
-                                        description={post.urlDescription}
-                                        image={post.urlImage}
-                                        message={post.userMessage}
-                                        name={post?.username}
-                                        profilePic={post?.profilePic}
-                                    />
-                                )
-                            )
+                                ? "There are no posts yet"
+                                : error === true
+                                    ? "An error occured while trying to fetch the posts, please refresh the page"
+                                    :
+                                    (
+                                        posts?.map((post) =>
+                                            <Post
+                                                key={post.id}
+                                                postId={post.id}
+                                                url={post.url}
+                                                title={post.urlTitle}
+                                                description={post.urlDescription}
+                                                image={post.urlImage}
+                                                message={post.userMessage}
+                                                name={post?.username}
+                                                profilePic={post?.profilePic}
+                                            />
+                                        )
+                                    )
                     }
                 </LeftWrapper>
                 <RightWrapper>
